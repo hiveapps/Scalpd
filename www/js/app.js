691c16793ca9c473310bac1cfc52ab41701ee753
atans2468 <austintans7@gmail.com>
2015-12-13 02:39:54 -0800

    adding in basic PayPal factory

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('scalpd', ['ionic', 'scalpd.controllers', 'scalpd.services', 'firebase'])
+angular.module('scalpd', ['ionic', 'scalpd.controllers', 'scalpd.services', 'firebase', 'appConstant'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
